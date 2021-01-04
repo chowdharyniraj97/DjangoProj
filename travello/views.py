99a17504ef4dfedb45b06ca7d21b57a959fdc8ed
Niraj Chowdhary <chowdharyniraj97@gmail.com>
2021-01-04 01:03:34 -0500

    added dynamic content

--- travello/views.py
+++ travello/views.py
@@ -1,6 +1,19 @@
 from django.shortcuts import render
-
+from .models import Destination
 # Create your views here.
 
 def index(request):
-    return render(request,'index.html')
+    dest1 = Destination()
+    dest1.name = "Mumbai"
+    dest1.price = 700
+    dest1.desc= "Amachi Mumbai"
+    dest1.img = "destination_1.jpg"
+
+    dest2 = Destination()
+    dest2.name = "Bengaluru"
+    dest2.price = "800"
+    dest2.desc = "Just code here"
+    dest2.img = "destination_2.jpg"
+
+    dests = [dest1, dest2]
+    return render(request,'index.html', {'dests' : dests})
